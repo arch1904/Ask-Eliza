666b3ebce7fc32d3535ee78e30d0cfc54435cf7f
ArkinDharawat <arkin.dharawat@gmail.com>
2017-01-21 17:15:48 -0500

    app.py added

--- mongo.py
+++ mongo.py
@@ -2,20 +2,16 @@ import pymongo
 from pymongo import MongoClient
 import datetime as dt
 
-client = MongoClient()
+client = MongoClient('mongodb://localhost:27017/')
 
-db=client['User-Data']
+db = client['User-Data']
 collection = db['User-Data']
 posts = db.posts
 def add_user(name,email,comments):
 	curr_user = {"name": name,
 	 "date": dt.datetime.utcnow(),
-	 "email":email, 
+	 "email":email,
 	 "comments": comments}
 
 	posts = db.posts
 	post_id = posts.insert_one(curr_user).inserted_id
-
-
-
-
