c258cec2d388ffe209800ae98a47d8b43ff28d8f
Archit Gupta <archit.941@gmail.com>
2017-01-21 13:45:51 -0500

    Make Transcripts better

--- elizaAlexa.py
+++ elizaAlexa.py
@@ -15,24 +15,24 @@ ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 name = "Archit"
 f = open("Transcript_"+name+".txt",'a')
-f.write(str(dt.datetime.now())+"\n")
+f.write(str(dt.datetime.now())+"\n\n")
 
 @ask.launch
 def begin_session():
 	start_msg = render_template('start')
-	f.write(start_msg+"\n")
+	f.write("Eliza: "+start_msg+"\n")
 	return question(start_msg)
 
 @ask.intent("ResponseIntent",convert={'answer':str})
 def start_conversation(answer):
 	#f = open('templates.yaml','a')
 	session.attributes['answers'] = answer
-	f.write(answer+"\n")
+	f.write("You: "+answer+"\n")
 	eliza_response = ez.analyze(session.attributes['answers'])
 	#f.write("curr_response: " + eliza_response)
 	#f.close()
 	curr_response_msg = str(eliza_response)
-	f.write(curr_response_msg+"\n")
+	f.write("Eliza: "+curr_response_msg+"\n")
 	return question(curr_response_msg)
 
 if __name__=='__main__':
